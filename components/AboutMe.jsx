7ea58f939062667bedf80f1dabd677d05cb9d432
Jun Shen <108778342+Junshen18@users.noreply.github.com>
2023-12-10 16:03:32 +0800

    Major modification for portfolio website

---
+++ components/AboutMe.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { useTransition, useState } from "react";
+import TabButton from "./TabButton";
+import { Quicksand } from 'next/font/google'
+ 
+const quicksand = Quicksand({
+  weight: '700',    
+  subsets: ['latin'],
+})
+
+const technologies = [
+    'React',
+    'Flutter',
+    'Next.js',
+    'JavaScript',
+    'HTML',
+    'Vue.js',
+    'R',
+    'CSS',
+    'Tailwind CSS',
+    'Python',
+  ];
+  
+  const technologyList = technologies.map((tech, index) => (
+    <li key={index}>{tech}</li>
+  ));
+  
+  
+
+//1d
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+            <ul className="list-disc pl-2">{technologyList}</ul>
+        ),
+    },
+    {
+        title: "Experiences",
+        id: "experiences",
+        content: (
+          <ul className="list-disc pl-2">
+            <li>Lv 99 Game Jam 2023</li>
+            <li>Virtual Innovation Competition 2023</li>
+            <li>UI/UX tournament 2023</li>
+            <li>ACIYLS Sustainability Startathon Malaysia 2023</li>
+          </ul>
+        ),
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+          <ul className="list-disc pl-2">
+                <li>Asia Pacific University</li>
+                <li>Catholic High School</li>
+                <li>SJK(C)Subang</li>
+          </ul>
+        ),
+    }
+];
+//1
+const AboutMe = () => {
+    //1b
+    const[tab, setTab] = useState("skills");
+    const[isPending, startTransition] = useTransition();
+    const handleTabChange = (id) => {
+        startTransition(() => {
+            setTab(id);
+        });
+    };
+    //1a
+    return(
+        <section className={`text-white ${quicksand.className} grid `} id="aboutMe">
+            <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-8 md:px-32 py-16 xl-gap-16 items-center">
+                <div className="place-self-center">
+                    <img src="/aboutme.jpg" alt="About Me" width={450} height={450} style={{ borderRadius: "25px" }}/>
+                </div>
+                <div className="mt-4 mx-10 lg:mx-0 md:mt-0 text-left flex flex-col h-full">
+                    <h1 className="text-4xl font-bold text-white mb-4">About Me</h1> 
+                    <p className="text-base text-justify lg:text-lg">
+                        I am currently pursuing a degree in Software Engineering at APU, where I have been honing my skills in various technologies. I am interested in exploring various kinds of IT topics, including Web 3 industry, and AI technology.
+                        My passion for technology and problem-solving has led me to voluntarily participate in more hackathons in the future, where I primarily serve as a Front End Developer, and Part time Artist. However, I am not limited to frontend tasks and often take on backend responsibilities, demonstrating my versatility and commitment to delivering complete solutions. </p>
+                    <div className="flex flex-row mt-8">
+                        <TabButton 
+                        selectTab={() => handleTabChange("skills")}
+                        active= {tab === "skills"}
+                        >   
+                            Skills
+                        </TabButton>
+
+                        <TabButton 
+                        selectTab={() => handleTabChange("experiences")}
+                        active= {tab === "experiences"}
+                        >
+                            {" "}
+                            Experiences{" "}
+                        </TabButton>
+
+                        <TabButton 
+                        selectTab={() => handleTabChange("education")}
+                        active= {tab === "education"}
+                        >
+                            {" "}
+                            Education{" "}
+                        </TabButton>
+                        
+                    </div>
+                    <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
+                </div>
+
+            </div>
+        </section>
+
+    );
+};
+export default AboutMe;
