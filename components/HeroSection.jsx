9773b1319e434db1616b14ae9a8f30eab3ac365d
Junshen18 <108778342+Junshen18@users.noreply.github.com>
2024-06-12 00:19:36 +0800

    hero section modified

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -27,14 +27,14 @@ const HeroSection = () => {
         >
           <h1 className="font-extrabold">
             {/* span text seperator -- for design*/}
-            <span className="text-white text-7xl md:text-5xl sm:text-7xl">
+            <span className="text-white text-[40px] leading-none md:text-5xl lg:text-7xl">
               Hello, I&apos;m{" "}
-              <span className="text-transparent bg-clip-text sm:text-8xl md:text-8xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+              <span className="text-transparent bg-clip-text text-[50px] leading-none md:text-8xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
                 Jun Shen
               </span>
             </span>
             <br></br>
-            <span className="text-6xl md:text-5xl">
+            <span className="text-4xl md:text-5xl">
               I&apos;m a
               <TypeWritter
                 options={{
@@ -59,21 +59,21 @@ const HeroSection = () => {
               ></TypeWritter>
             </span>
           </h1>
-          <p className="text-gray-300 pt-8 text-base sm:text-lg mb-6 lg:text-xl">
+          <p className="text-gray-300 pt-8 text-base mb-6 lg:text-2xl">
             Slide down to know more about me!
           </p>
           <Link
             href="#projects"
-            className="px-12 mb-4 inline-block py-3 rounded-full mr-4 border-solid border-2 border-indigo-600 hover:bg-violet-600"
+            className="px-10 mb-4 inline-block py-3 rounded-full mr-4 border-solid border-2 border-indigo-600 hover:bg-violet-600"
           >
-            <span className="text-xl text-white">Projects</span>
+            <span className="text-base md:text-xl text-white">Projects</span>
           </Link>
-          <div className="px-6 inline-block py-3 rounded-full mr-4 border-solid border-2  border-white hover:bg-white hover:text-black text-xl text-white">
+          <div className="px-9 md:px-12 inline-block py-3 rounded-full mr-4 border-solid border-2  border-white hover:bg-white hover:text-black text-xl text-white">
             <a
               href="/Wong-Jun-Shen-Resume.pdf"
               download="Wong-Jun-Shen-Resume.pdf"
             >
-              <button>View My Resume</button>
+              <button className="text-base md:text-xl">View My Resume</button>
             </a>
           </div>
         </motion.div>
@@ -85,13 +85,13 @@ const HeroSection = () => {
             transition={{ duration: 0.5 }}
           >
             <div
-              className="lg:w-[400px] lg:h-[400px] relative z-0 md:w-[300px] md:h-[300px] h-[400px] w-[400px] "
+              className="lg:w-[400px] lg:h-[400px] relative z-0 md:w-[300px] md:h-[300px] h-[300px] w-[300px] "
               onMouseEnter={handleHover}
               onMouseLeave={handleHover}
             >
               <Image
-                src={isHovered ? "/852364.jpeg" : "/IMG_0907.jpg"}
-                alt="hero section"
+                src={isHovered ? "/IMG_0907.jpg" : "/IMG_0204.jpg"}
+                alt="profilepic"
                 className=" transform rounded-full flex relative -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                 width={450}
                 height={450}
