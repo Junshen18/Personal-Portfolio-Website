40eaab52d04f5432d7e51a619d750ecc37c89207
Junshen18 <108778342+Junshen18@users.noreply.github.com>
2024-06-11 16:28:59 +0800

    add motion Background

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,30 @@
-import dynamic from 'next/dynamic';
-import React from 'react';
-import AboutMe from '../components/AboutMe';
-import Projects from '../components/Projects';
-import EmailSection from '../components/EmailSection';
-import Footer from '../components/Footer';
-import AchievementsSection from '../components/AchievementsSection';
+import dynamic from "next/dynamic";
+import React from "react";
+import AboutMe from "../components/AboutMe";
+import Projects from "../components/Projects";
+import EmailSection from "../components/EmailSection";
+import Footer from "../components/Footer";
+import AchievementsSection from "../components/AchievementsSection";
+import Background from "@/components/Background";
 
 //Server: not found error handler
-const HeroSection = dynamic(
-  () => import('../components/HeroSection'),
-  { ssr: false }
-);
+const HeroSection = dynamic(() => import("../components/HeroSection"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
-        <main>
+    <div className="flex min-h-screen flex-col blobs">
+      <Background />
+
+      <main>
         <HeroSection />
         <AchievementsSection />
         <AboutMe />
         <Projects />
         <EmailSection />
-        </main>
-        <Footer />
+      </main>
+      <Footer />
     </div>
   );
 }
